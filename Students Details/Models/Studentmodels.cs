ac61ebc1c85ce52d18b034a1fc7e8fccf5b896fb
prathap <prathapsakthivel.ps@gmail.com>
2023-08-14 13:03:59 +0530

    Add project files.

---
+++ Students Details/Models/Studentmodels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Students_Details.Models
+{
+    public class Studentmodels
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public  string Mailid { get; set; }
+        public long Mobilenumber { get; set; }
+        public string classname { get; set; }
+        public string Fathername { get; set; }
+        public string Mothername { get; set; }
+
+    }
+}
